bdd31453e21c2768814314b3a952fb642b9bbd9f
Matteo Smerlak <matteo.smerlak@gmail.com>
2022-04-13 10:49:58 +0200

    before we change production and threshold

--- scripts/replicas.jl
+++ scripts/replicas.jl
@@ -3,80 +3,74 @@ using DrWatson
 
 using UnPack
 using QuadGK, FastGaussQuadrature
-using NLsolve
+using NLsolve, SpeedMapping
 using LinearAlgebra
 
 function H_RS(x, f, p)
-    @unpack  ρ, μ, σ, β, V, λ = p
+    @unpack  μ, σ, β, V= p
     n, z = x
     qd, q0, h = f
-    @assert qd >= q0 "qd < q0"
-    return - ρ^2*σ^2*β*(qd-q0)*n^2/2 + (ρ*μ*h-z*ρ*sqrt(q0)*σ)*n + V(n) #+ (1/β-λ)*log(n)
+    return - σ^2*β*(qd-q0)*n^2/2 + (μ*h-z*sqrt(q0)*σ)*n + V(n) #+ (1/β-λ)*log(n)
+end
+
+function int(f, a, b)
+    x, w = gausslegendre(5)
+    d = (b-a)/2
+    s = (b+a)/2
+    return d*dot(w, f.(d*x .+ s)) 
 end
 
 function F(f, p; n_min = 1e-2, n_max = 100, quadrature = gausshermite(3))
-    @unpack  ρ, μ, σ, β, V, λ = p
+
+    @unpack  μ, σ, β, V= p
     x, w = quadrature
 
     m(n, z) = exp(-β*H_RS([n, z], f, p))
 
-    Z(z) = quadgk(n -> m(n, z), n_min, n_max)[1]
-    N(z) = quadgk(n -> n*m(n, z), n_min, n_max)[1]
-    N2(z) = quadgk(n -> n^2*m(n, z), n_min, n_max)[1]
+    # Z(z) = quadgk(n -> m(n, z), n_min, n_max)[1]
+    # N(z) = quadgk(n -> n*m(n, z), n_min, n_max)[1]
+    # N2(z) = quadgk(n -> n^2*m(n, z), n_min, n_max)[1]
 
-    I1(z) = N2(z)/Z(z)
-    I2(z) = (N(z)/Z(z))^2
-    I3(z) = N(z)/Z(z)
+    Z(z) = int(n -> m(n, z), n_min, n_max)
+    N(z) = int(n -> n*m(n, z), n_min, n_max)
+    N2(z) = int(n -> n^2*m(n, z), n_min, n_max)
+
+    println(Z(.1))
+    I1(z) = N2(z*sqrt(2))/Z(z*sqrt(2))
+    I2(z) = (N(z*sqrt(2))/Z(z*sqrt(2)))^2
+    I3(z) = N(z*sqrt(2))/Z(z*sqrt(2))
 
     # qd, q0, h = F
-    return (1/sqrt(2π))*[dot(w, Ik.(x)) for Ik in (I1, I2, I3)]
+    return (1/sqrt(π))*[dot(w, Ik.(x)) for Ik in (I1, I2, I3)]
 end
 
 
-function F!(F, f, p; n_min = 1e-2, n_max = 100, quadrature = gausshermite(3))
-    @unpack  ρ, μ, σ, β, V, λ = p
-    x, w = quadrature
+plot(n -> H_RS([n, .1], [1., .2, .1], p))
+F([1., .2, .1], p; n_min = 1., n_max = 3)
 
-    m(n, z) = exp(-β*H_RS([n, z], f, p))
+function replicon(p; kwargs...)
 
-    Z(z) = quadgk(n -> m(n, z), n_min, n_max)[1]
-    N(z) = quadgk(n -> n*m(n, z), n_min, n_max)[1]
-    N2(z) = quadgk(n -> n^2*m(n, z), n_min, n_max)[1]
-
-    I1(z) = (1/sqrt(2π))*N2(z)/Z(z)
-    I2(z) = (1/sqrt(2π))*(N(z)/Z(z))^2
-    I3(z) = (1/sqrt(2π))*N(z)/Z(z)
-
-    # qd, q0, h = F
-    F[1] = dot(w, I1.(x))
-    F[2] = dot(w, I2.(x))
-    F[3] = dot(w, I3.(x))
-end
+    @unpack  σ, β = p
 
 
-function find_fp(p; kwargs...)
-    @unpack  ρ, σ, β = p
-    fp = fixedpoint((F, f) -> F!(F, f, p; kwargs...), [2., 1., 1.]; method = :anderson)
+    fp = fixedpoint(f -> F(f, p; kwargs...), [.5, .4, .5]; method = :anderson, m = 10, beta = .1, store_trace = true)
+    
+    @assert fp.f_converged "FP iteration didn't converge"
+
     qd, q0, h = fp.zero
-    @assert qd >= q0 "ouch"
-    return (qd, q0, h)
-    #return (β*ρ*σ)^2(1 -  (β*ρ*σ)^2*(qd - q0)^2)
+    @show [qd, q0, h]
+    return (β*σ)^2(1 - (β*σ)^2*(qd - q0)^2)
 end
-
 
 p = (
-    ρ = 1.,
-    β = 30.,
-    μ = 2.,
-    σ = 0.01,
-    λ = 1.,
+    β = 50.,
+    μ = 5.,
+    σ = 0.3,
     V = n -> - (4/3)n^(3/4),
 )
 
-F(
-    [0.41742, 0.35669, 0.58943], # (qd, q0, h)
-    p;
-    n_min = 0, n_max = 100, quadrature = gausshermite(5)    
-)
+F([.5, .1, .1], p)
 
-find_fp(p)
+replicon(p; n_max = 100)
+
+# Ada's numbers = [0.41742, 0.35669, 0.58943]
--- src/dynamics.jl
+++ src/dynamics.jl
@@ -2,22 +2,6 @@ using DifferentialEquations
 using Random, Distributions
 using LinearAlgebra
 
-# function F(x, p)
-#     # the vector field
-#     f = .- x.^2/p[:K]^(2 - p[:k]) .- x.*(p[:a]*x) .+ p[:λ]
-
-#     above_threshold = x .> p[:n0]
-#     f[above_threshold] .+= p[:r][above_threshold].*x[above_threshold].^p[:k]
-#     return f
-# end
-
-# function F!(f, x, p)
-#     x .= ppart.(x)
-#     f .= .- x.^2/p[:K]^(2 - p[:k]) .- x.*(p[:a]*x) .+ p[:λ]
-
-#     above_threshold = x .> p[:n0]
-#     f[above_threshold] .+= p[:r][above_threshold].*x[above_threshold].^p[:k]
-# end
 
 function production(x, p)
         return (x > p[:n0] ? x^p[:k]*p[:n0]^(1-p[:k]) : x) - x^2/p[:K]

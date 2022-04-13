c6fd04a0c68dcc9eaeb68459f36c5947c0802685
Onofrio Mazzarisi <onofrio.mazzarisi@gmail.com>
2022-04-13 17:37:55 +0200

    add overprint option to boundary()

--- src/spectra.jl
+++ src/spectra.jl
@@ -15,7 +15,7 @@ T(x, y, n, p) = (q = (p[:μ]-1/p[:K]^(2-p[:k]));
 
 
 
-function boundary(p)
+function boundary(p; overprint = false)
 
     if !haskey(p, :a) random_interactions!(p) end
     if !haskey(p, :equilibrium) evolve!(p) end
@@ -33,10 +33,15 @@ function boundary(p)
         # println(T(0, 0, n, p) - 1/(p[:σ])^2)
         # println( p[:S]/((1-p[:k])p[:μ]p[:S])^2 - 1/(p[:σ])^2 )
         X = range(minimum(real.(s)), maximum(real.(s)); length = 100)
-        Y = range(1.1*minimum(imag.(s)), 1.1*maximum(imag.(s)); length =  100)
+        Y = range(minimum(imag.(s)), maximum(imag.(s)); length =  100)
 
-        scatter!(s, alpha = .3, legend = false,
-        aspect_ratio = 1)
+        if overprint
+            scatter!(s, alpha = .3, legend = false,
+            aspect_ratio = 1, grid = false)
+        else
+            scatter(s, alpha = .3, legend = false,
+            aspect_ratio = 1, grid = false)
+        end
 
         Plots.contour!(X, Y, (x,y) -> T(x, y, n, p), levels = [1/(p[:σ])^2],
         linewidth = 2, color = :auto, colorbar = false)

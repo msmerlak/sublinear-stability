0a23dea5929beca77dc501441d326f9f340e83a9
Onofrio Mazzarisi <onofrio.mazzarisi@gmail.com>
2022-04-20 15:02:41 +0200

    add colors of different darkness

--- src/constants.jl
+++ src/constants.jl
@@ -1,5 +1,11 @@
 COLOR_LOG = :turquoise3
 COLOR_SUB = :darkgoldenrod1
 
+COLOR_LOG49 = "#009af9"
+COLOR_SUB49 = "#fb8d00"
+
+COLOR_LOG35 = "#0070b2"
+COLOR_SUB35 = "#b26300" 
+
 K = 100.
 CUTOFF = 1.
